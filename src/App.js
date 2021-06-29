f67e21078de5a70f5d570715682f5d7f734a7728
MattCross01 <mountaindew2141@outlook.com>
2021-06-29 13:31:39 -0400

    working on components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Jumbotron from './components/Jumbotron';
 import Navbar from './components/Navbar';
-import About from './components/About';
+import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Certificat from './components/Certificat';
+import Certificate from './components/Certificate';
 import ScrollUpButton from './components/ScrollUpButton';
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
       <ScrollUpButton />
       <Navbar />
       <Jumbotron />
-      <About />
-      <Certificat />
+      <AboutMe />
+      <Certificate />
       <Portfolio />
       <Contact />
       <Footer />
---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import Resume from './resume/TolgaSecmeNewResumeSecondUpdate.pdf';
+
+
+function About() {
+    return (
+        <section className="bg-light about-page" id="about">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-8 text-center">
+                        <h2 className="text-center mt-0 about">About Me</h2>
+                        <hr className="divider my-4" color="#408eff" />
+                        <p className="text-muted mb-0">Matthew Cross is a Student Full-Stack Web Developer with Front End and Back End development experience. My current job interest is becoming a SQL Developer. I personally love working with databases and routing.</p>
+                            <div>
+                                <a href={Resume} target="blank" className="btn btn-outline-dark about-btn" role="button"><i class="fas fa-user-circle"></i> Resume</a>
+                                <a href="https://github.com/MattCross01" className="btn btn-dark about-btn btn-outline-light" target="blank" role="button"><i className="fab fa-github"></i> Github</a>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
